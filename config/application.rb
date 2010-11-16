2db9980cccb3603d309476e44c7111d20a918d65
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-17 00:43:18 +0700

    first commit

--- config/application.rb
+++ config/application.rb
@@ -32,12 +32,13 @@ module Purpleline
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui application rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui application rails ckeditor)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.autoload_paths += %W( #{config.root}/app/models/ckeditor )
   end
 end
