c2354326944afa40e147cea02c5fb46862825df9
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-16 23:49:12 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purpleline::Application.config.secret_token = '28918a306efcf71e8466f5fc2cb0c68dd7f4b7d4c5862fb729c92ac7a96a8d534c3123eecfb4a12ccf443f0153ee930e1465d5098712129bc4477fcd309ba06d'
