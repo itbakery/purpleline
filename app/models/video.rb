a1f4bd69d8530c8240c6b295cf20bccf6bd0ff3b
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-22 19:45:21 +0700

    commit

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,2 @@
-class Image < PurpleAssetResource
-  has_attached_file  :file,
-    :styles => {
-      :thumbnil => '180x180',
-      :ipod => ['320x480',:mp4]
-    },
-    :processors => "video_thumbnail"
+class Video < PurpleAssetResource
 end
