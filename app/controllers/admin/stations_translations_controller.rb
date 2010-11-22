18b89c9378aab90b92004b85ae650d2767f221b4
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-22 22:13:29 +0700

    commit

--- app/controllers/admin/stations_translations_controller.rb
+++ app/controllers/admin/stations_translations_controller.rb
@@ -55,8 +55,8 @@ class Admin::StationsTranslationsController < ApplicationController
       var tmp = latlng.split(',');
       var px = tmp[0];
       var py = tmp[1];
-      $('stations_translation_latitude').val(px);
-      $('stations_translation_longtitude').val(py);
+      $('#stations_translation_latitude').val(px);
+      $('#stations_translation_longtitude').val(py);
        }")
 
     respond_to do |format|
