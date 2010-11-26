773d48142c95efd1c9445f2b54d71123839426fd
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-26 07:19:42 +0700

    commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
+   $('#announces_translation_start_on').datepicker({numberOfMonths:2});
+   $('#announces_translation_stop_on').datepicker({numberOfMonts:2});
    $('#tabs').tabs();
-   $('#tabs').tabs('paging', { cycle: true, follow: true } );
+   $('#tabs').tabs('paging', { cycle: true, follow: true } );   
    $('#s3slider').s3Slider({timeOut: 9000 });
    $('#slider').s3Slider({timeOut: 9000});
-   $('#announces_translation_start_on').datepicker();
-   $('#announces_translation_stop_on').datepicker();
 });
 
 
