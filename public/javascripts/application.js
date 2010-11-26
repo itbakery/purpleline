cf2bd9bc908fcc74b13a0e7d5ec8772048a1484d
sawangpong muadphet <sawangpongm@gmail.com>
2010-11-26 08:41:11 +0700

    commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
    $('#announces_translation_start_on').datepicker({numberOfMonths:2});
-   $('#announces_translation_stop_on').datepicker({numberOfMonts:2});
+   $('#announces_translation_stop_on').datepicker({numberOfMonths:2});
    $('#tabs').tabs();
    $('#tabs').tabs('paging', { cycle: true, follow: true } );   
    $('#s3slider').s3Slider({timeOut: 9000 });
