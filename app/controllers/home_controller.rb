0ed5601b39b80d99b9f6ebcfd1d36f309386d38a
sawangpong muadphet <sawangpongm@gmail.com>
2010-12-05 16:15:52 +0700

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -67,8 +67,8 @@ class HomeController < ApplicationController
       f.legend({:layout=>"vertical",:align=>"right",:verticalAlign=> "top",:borderWidth =>'top',:style=>{:position=>'absolute', :bottom=>'auto', :left=>'0px', :top=>'20px'}})
       f.options[:x_axis][:categories] = @progresses.map(&:month)
       f.options[:x_axis][:labels] = {:rotation=>-90 , :align => 'right'}
-      f.options[:x_axis][:title] = {:text=> "Months"}
-      f.options[:y_axis][:title] = {:text=> "Project Progress"}
+      #f.options[:x_axis][:title] = {:text=> "Months"}
+      f.options[:y_axis][:title] = {:text=> "Project Progress (%)"}
       f.series(:type=> 'line',:name=> 'Schedule',:data=>  @progresses.map(&:schedule))
       f.series(:type=> 'line',:name=> 'Actual',:data=>  @progresses.map(&:actual))
     end
