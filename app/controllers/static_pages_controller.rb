daf65bf4467f90bd7524cc2197ad9383e8d9ca38
sawangpong muadphet <sawangpongm@gmail.com>
2010-12-16 16:46:20 +0700

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def page
-  	send_file  "#{Rails.root}/app/views/static_pages/#{params[:filename]}.#{params[:format]}",:disposition =>'inline',:type => StaticPage::Formats[params[:format]]
+  	send_file  "#{Rails.root.to_s}/app/views/static_pages/#{params[:filename]}.#{params[:format]}",:disposition =>'inline',:type => StaticPage::Formats[params[:format]]
   end
 end
