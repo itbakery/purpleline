95ad8cc069634237c743ce654e51ea60aa21f06a
sawangpong muadphet <sawangpongm@gmail.com>
2010-12-17 03:19:38 +0700

    commit

--- app/controllers/static_maps_controller.rb
+++ app/controllers/static_maps_controller.rb
@@ -1,10 +1,10 @@
 class StaticMapsController < ApplicationController
   def map
-    send_file  "#{Rails.root.to_s}/public/maps/#{params[:mapname]}/#{params[:filename]}.#{params[:format]}",:disposition =>'inline',:type => StaticMap::Formats[params[:format]]
+    send_file  "#{Rails.root.to_s}/public/map/#{params[:mapname]}/#{params[:filename]}.#{params[:format]}",:disposition =>'inline',:type => StaticMap::Formats[params[:format]]
   end
 
   def maptile
-    send_file  "/maps/#{params[:mapname]}/#{params[:z]}/#{params[:x]}/#{params[:y]}.#{params[:format]}",:disposition =>'inline',:type => StaticMap::Formats[params[:format]]
+    send_file  "#{Rails.root.to_s}/public/map/#{params[:mapname]}/#{params[:z]}/#{params[:x]}/#{params[:y]}.#{params[:format]}",:disposition =>'inline',:type => StaticMap::Formats[params[:format]]
   	
   end
 end
