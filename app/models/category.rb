c41f848bb4c8f607a74b4407d39463338ea0c25c
sawangpong muadphet <sawangpongm@gmail.com>
2010-12-19 17:41:49 +0700

    commit

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
 	has_and_belongs_to_many :announces
-  has_attached_file  :icon, :styles => {:iconsmall=>"32x32>"}
+  has_attached_file  :icon, :styles => {:iconsmall=>"32x32>"},
+  :url  => "/icons/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/icons/:id/:style/:basename.:extension"
+  validates_attachment_content_type :icon, :content_type => ['image/jpeg', 'image/png']
 end
