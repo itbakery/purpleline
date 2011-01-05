75d6aa6e1e1ee91bf1d57c6fe79a75e2baf352ec
sawangpong muadphet <sawangpongm@gmail.com>
2011-01-06 03:16:31 +0700

    commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+    function remove_fields(link) {
+       jQuery(link).prev("input[type=hidden]").val("1");
+       jQuery(link).closest(".fields").hide();
+     }
+
+    function add_fields(link, association, content) {
+       var new_id = new Date().getTime();
+       var regexp = new RegExp("new_" + association, "g")
+       jQuery(link).parent().before(content.replace(regexp, new_id));
+    }
+
 
  jQuery(document).ready(function() {
     jQuery("#accordion" ).accordion({
@@ -55,16 +66,6 @@
     
     jQuery('#tabs').tabs();
     
-    function remove_fields(link) {
-  jQuery(link).prev("input[type=hidden]").val("1");
-  jQuery(link).closest(".fields").hide();
-}
-
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
-  jQuery(link).parent().before(content.replace(regexp, new_id));
-}
 
 
 });
