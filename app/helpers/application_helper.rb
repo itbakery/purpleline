cf94f2a70df727c2002a31404d632fa82b913334
sawangpong muadphet <sawangpongm@gmail.com>
2011-01-06 02:26:02 +0700

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,16 @@ module ApplicationHelper
     orig_select_tag(name, new_select_options.html_safe, options.merge(html_options))
   end
 
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -58,13 +58,13 @@
 });
 
 function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
+  jQuery(link).prev("input[type=hidden]").val("1");
+  jQuery(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).parent().before(content.replace(regexp, new_id));
+  jQuery(link).parent().before(content.replace(regexp, new_id));
 }
 
