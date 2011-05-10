ef77b82a7267e5c599ba112951c757127909067e
sawangpong muadphet <sawangpongm@gmail.com>
2011-05-10 19:08:26 +0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110423083107) do
+ActiveRecord::Schema.define(:version => 20110509084757) do
 
   create_table "announces", :force => true do |t|
     t.string   "name"
