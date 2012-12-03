5e0c0d4df9bf277b9ca9e839cb5435a22e5407b0
sawangpongm@gmail.com <sawangpongm@gmail.com>
2012-12-04 05:21:45 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repository,  "git@github.com:itbakery/purpleline.git"
 set :user, "admin"
 set :scm, :git
 set :run_method, :run
-set :deploy_to, "/home/admin/#{application}"
+set :deploy_to, "/home/#{application}"
 set :scm_verbose,true
 set :branch, "master"
 #==== intetration with capistrano
@@ -13,7 +13,7 @@ require 'rvm/capistrano'
 set :rvm_type, :user
 set :rvm_ruby_string, "1.9.3"
 set :rvm_bin_path, "/home/admin/.rvm/bin"
-#set :rvm_ruby_string, "ruby-1.9.3-p286@purpleline"
+set :rvm_ruby_string, "ruby-1.9.3-p286@purpleline"
 #set :rvm_ruby_string, "ruby-1.9.3-p327@purpleline"
 
 set :deploy_via, :remote_cache
