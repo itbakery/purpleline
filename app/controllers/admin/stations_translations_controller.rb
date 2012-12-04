ce117f815c3c26dcfab01ce7d045b3442dd9f654
sawangpongm@gmail.com <sawangpongm@gmail.com>
2012-12-04 14:29:51 +0700

    commit

--- app/controllers/admin/stations_translations_controller.rb
+++ app/controllers/admin/stations_translations_controller.rb
@@ -1,7 +1,7 @@
 class Admin::StationsTranslationsController < ApplicationController
   layout "admin"
   before_filter :authenticate_user!  
-  include ActionView::Helpers::PrototypeHelper
+  #include ActionView::Helpers::PrototypeHelper
   include ActionView::Helpers::JavaScriptHelper
   include ActionView::Helpers::TagHelper
   # GET /stations_translations
