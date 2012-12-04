51e4122ea654b0b50801168df20e45c29ce341fa
sawangpongm@gmail.com <sawangpongm@gmail.com>
2012-12-04 14:25:04 +0700

    commit

--- app/controllers/admin/announces_translations_controller.rb
+++ app/controllers/admin/announces_translations_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AnnouncesTranslationsController < ApplicationController
   layout "admin"
   before_filter :authenticate_user!
-  include ActionView::Helpers::PrototypeHelper
+  #include ActionView::Helpers::PrototypeHelper
   include ActionView::Helpers::JavaScriptHelper
   include ActionView::Helpers::TagHelper
   # GET /announces_translations
