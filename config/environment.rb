ea635e5425dac8424afdca32288b8913ae7c47de
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-09 03:19:27 +0700

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Purpleline::Application.initialize!
 Paperclip.options[:command_path] = "/usr/local/bin"
-config.gem(
-  'thinking-sphinx',
-  :lib     => 'thinking_sphinx',
-  :version => '3.0.0'
-)
