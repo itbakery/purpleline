6e2d341f1ba4cf7489a96fa1cb7be9a5c7618040
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-09 03:51:45 +0700

    commit

--- app/models/reports_translation.rb
+++ app/models/reports_translation.rb
@@ -4,9 +4,9 @@ class ReportsTranslation < ActiveRecord::Base
 	cattr_reader :per_page
     @@per_page = 20
     has_many :images, :as=>:assetable, :class_name=>'Report::Image',:dependent => :destroy
-    accepts_nested_attributes_for :images, :reject_if => lambda {|a| a[:attachment].blank?},:allow_destroy => true   
-    define_index do 
-  	  indexes :title
-  	  indexes :content      
-    end 
+    accepts_nested_attributes_for :images, :reject_if => lambda {|a| a[:attachment].blank?},:allow_destroy => true
+  #  define_index do
+  #	  indexes :title
+  #	  indexes :content
+  #  end
 end
