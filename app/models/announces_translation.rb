eec398184124c5a57dbbb7c98fe500ee2a4ecda6
sawangpongm@gmail.com <sawangpongm@gmail.com>
2013-01-09 02:26:04 +0700

    commi

--- app/models/announces_translation.rb
+++ app/models/announces_translation.rb
@@ -2,8 +2,8 @@ class AnnouncesTranslation < ActiveRecord::Base
 	belongs_to   :announce
 	cattr_reader :per_page
   @@per_page = 20
-  define_index do 
+  define_index do
   	indexes :title
-  	indexes :content      
+  	indexes :content
   end
 end
