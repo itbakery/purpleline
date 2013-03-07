6d0b3ba6aa29f1576a49c7428df7cf3935399493
sawangpong muadphet <sawangpongm@gmail.com>
2013-03-07 13:54:08 +0700

    commit

--- app/models/reports_reports_types.rb
+++ app/models/reports_reports_types.rb
@@ -1,5 +1,4 @@
 class ReportsReportsTypes < ActiveRecord::Base
   belongs_to :report
   belongs_to :reports_type
-  named_scope :date_current, :conditions => ["Date(start_on) <= Date(NOW()) AND Date(stop_on) >=  Date(NOW())"]
 end
