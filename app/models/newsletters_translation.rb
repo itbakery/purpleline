6267c184e0051bae0b570d0293083a0127e1f67f
sawangpong muadphet <sawangpongm@gmail.com>
2013-03-07 14:04:34 +0700

    commit

--- app/models/newsletters_translation.rb
+++ app/models/newsletters_translation.rb
@@ -1,7 +1,8 @@
 class NewslettersTranslation < ActiveRecord::Base
 	cattr_reader :per_page
   @@per_page = 10
-  scope :date_current, :conditions => ["Date(start_on) <= Date(NOW()) AND Date(stop_on) >=  Date(NOW())"]
+  #scope :date_current, :conditions => ["Date(start_on) <= Date(NOW()) AND Date(stop_on) >=  Date(NOW())"]
+  scope :date_current, :conditions => ["Date(start_on) <= Date(NOW())"]
 #  define_index do
 #  	indexes :title
 #  	indexes :content
