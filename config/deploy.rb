184b01a06a68b654d01813219d379e52e1d9435a
sawangpong muadphet <sawangpongm@gmail.com>
2013-03-07 13:44:44 +0700

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ require 'rvm/capistrano'
 set :rvm_type, :user
 set :rvm_ruby_string, "1.9.3"
 set :rvm_bin_path, "/home/admin/.rvm/bin"
-set :rvm_ruby_string, "ruby-1.9.3-p374@purpleline"
+set :rvm_ruby_string, "ruby-1.9.3-p385@purpleline"
 #set :rvm_ruby_string, "ruby-1.9.3-p327@purpleline"
 
 set :deploy_via, :remote_cache
